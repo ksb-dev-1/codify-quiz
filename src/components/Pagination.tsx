fababaa717fae5d0ac72d48d91baf7f15d42b28e
ksb-dev-1 <ksbabaleshwar@gmail.com>
2025-03-06 22:41:37 +0530

    some style change

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -3,16 +3,11 @@
 import { useRouter, useSearchParams } from "next/navigation";
 
 interface PaginationProps {
-  //questionsLoading: boolean;
   currentPage: number;
   totalPages: number;
 }
 
-const Pagination = ({
-  //questionsLoading,
-  currentPage,
-  totalPages,
-}: PaginationProps) => {
+const Pagination = ({ currentPage, totalPages }: PaginationProps) => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
@@ -40,7 +35,7 @@ const Pagination = ({
             currentPage === 1
               ? "cursor-not-allowed text-[#999]"
               : "cursor-pointer bg-primary text-white hover:bg-hover transition-colors"
-          } border px-4 py-2 mx-1 rounded-custom`}
+          } border px-4 py-2 mr-1 rounded-custom`}
         >
           Prev
         </button>
@@ -64,67 +59,11 @@ const Pagination = ({
             currentPage === totalPages
               ? "cursor-not-allowed text-[#999]"
               : "cursor-pointer bg-primary text-white hover:bg-hover transition-colors"
-          } border px-4 py-2 mx-1 rounded-custom`}
+          } border px-4 py-2 ml-1 rounded-custom`}
         >
           Next
         </button>
       </div>
-      {/* {questionsLoading ? (
-        <div className="text-transparent">
-          <button className="skeleton px-4 py-2 mx-1 border border-transparent rounded-custom">
-            Prev
-          </button>
-          {[1, 2, 3].map((_, index) => (
-            <button
-              key={index}
-              className="skeleton px-4 py-2 mx-1 border border-transparent rounded-custom"
-            >
-              {index}
-            </button>
-          ))}
-          <button className="skeleton px-4 py-2 mx-1 border border-transparent rounded-custom">
-            Next
-          </button>
-        </div>
-      ) : (
-        <div>
-          <button
-            onClick={() => handlePageChange(currentPage - 1)}
-            disabled={currentPage === 1}
-            className={`${
-              currentPage === 1
-                ? "cursor-not-allowed text-[#999]"
-                : "cursor-pointer bg-primary text-white hover:bg-hover transition-colors"
-            } border px-4 py-2 mx-1 rounded-custom`}
-          >
-            Prev
-          </button>
-          {getVisiblePages().map((visiblePage) => (
-            <button
-              key={visiblePage}
-              onClick={() => handlePageChange(visiblePage)}
-              className={`${
-                currentPage === visiblePage
-                  ? "cursor-pointer bg-primary text-white hover:bg-hover"
-                  : "hover:bg-slate-200"
-              } border px-4 py-2 mx-1 rounded-custom transition-colors`}
-            >
-              {visiblePage}
-            </button>
-          ))}
-          <button
-            onClick={() => handlePageChange(currentPage + 1)}
-            disabled={currentPage === totalPages}
-            className={`${
-              currentPage === totalPages
-                ? "cursor-not-allowed text-[#999]"
-                : "cursor-pointer bg-primary text-white hover:bg-hover transition-colors"
-            } border px-4 py-2 mx-1 rounded-custom`}
-          >
-            Next
-          </button>
-        </div>
-      )} */}
     </div>
   );
 };
